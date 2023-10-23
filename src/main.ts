a1b7a435080c656e7b502520c4781be8ed86db7d
jazze13 <jazze.hang@gmail.com>
2023-10-23 23:26:02 +0300

    Startup config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const PORT = process.env.SERVER_PORT;
+    const app = await NestFactory.create(AppModule);
+
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('Chats')
+        .setDescription('API Documentation')
+        .setVersion('1.0.0')
+        .build();
+
+    const swagger = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api/docs', app, swagger);
+
+    await app.listen(PORT, () => {
+        console.log(`Server started on port ${PORT}`);
+    });
 }
 bootstrap();
